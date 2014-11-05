50a644d317a9a9d859c6a004c67d159169782de1
kelseyb <kelsey.bellew@mines.sdsmt.edu>
2014-11-05 16:43:56 -0700

    can't seem to add database (not that its complete)

---
+++ Hospital/ExampleF14/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ExampleF14
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPatients = new System.Windows.Forms.Button();
+            this.btnPhysicians = new System.Windows.Forms.Button();
+            this.btnDetails = new System.Windows.Forms.Button();
+            this.btnProducts = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPatients
+            // 
+            this.btnPatients.Location = new System.Drawing.Point(83, 48);
+            this.btnPatients.Name = "btnPatients";
+            this.btnPatients.Size = new System.Drawing.Size(107, 23);
+            this.btnPatients.TabIndex = 0;
+            this.btnPatients.Text = "Patients";
+            this.btnPatients.UseVisualStyleBackColor = true;
+            this.btnPatients.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnPhysicians
+            // 
+            this.btnPhysicians.Location = new System.Drawing.Point(83, 88);
+            this.btnPhysicians.Name = "btnPhysicians";
+            this.btnPhysicians.Size = new System.Drawing.Size(107, 23);
+            this.btnPhysicians.TabIndex = 1;
+            this.btnPhysicians.Text = "Physicians";
+            this.btnPhysicians.UseVisualStyleBackColor = true;
+            // 
+            // btnDetails
+            // 
+            this.btnDetails.Location = new System.Drawing.Point(83, 130);
+            this.btnDetails.Name = "btnDetails";
+            this.btnDetails.Size = new System.Drawing.Size(172, 23);
+            this.btnDetails.TabIndex = 2;
+            this.btnDetails.Text = "Treatments maybe?";
+            this.btnDetails.UseVisualStyleBackColor = true;
+            // 
+            // btnProducts
+            // 
+            this.btnProducts.Location = new System.Drawing.Point(83, 172);
+            this.btnProducts.Name = "btnProducts";
+            this.btnProducts.Size = new System.Drawing.Size(107, 23);
+            this.btnProducts.TabIndex = 3;
+            this.btnProducts.Text = "dunno";
+            this.btnProducts.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(204, 213);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(51, 21);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Hospital Database";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnProducts);
+            this.Controls.Add(this.btnDetails);
+            this.Controls.Add(this.btnPhysicians);
+            this.Controls.Add(this.btnPatients);
+            this.Name = "MainForm";
+            this.Text = "Main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPatients;
+        private System.Windows.Forms.Button btnPhysicians;
+        private System.Windows.Forms.Button btnDetails;
+        private System.Windows.Forms.Button btnProducts;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Hospital/ExampleF14/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExampleF14
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        //shouldn't you be btnPatients??
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Form2 frmC = new Form2();
+
+                frmC.ShowDialog(this);
+                frmC.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in Main Form - " + ex.Message);
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
