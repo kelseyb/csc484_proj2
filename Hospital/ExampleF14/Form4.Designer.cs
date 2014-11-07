c62a8bfb7f045c053555798bac902c1075e77872
kelseyb <kelsey.bellew@mines.sdsmt.edu>
2014-11-07 14:36:20 -0700

    added pseudo code and functionality to form4

--- Hospital/ExampleF14/Form4.Designer.cs
+++ Hospital/ExampleF14/Form4.Designer.cs
@@ -28,11 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPatient = new System.Windows.Forms.TextBox();
+            this.txtPhysician = new System.Windows.Forms.TextBox();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDosage = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Patient";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(223, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Physician";
+            // 
+            // txtPatient
+            // 
+            this.txtPatient.Location = new System.Drawing.Point(68, 34);
+            this.txtPatient.Name = "txtPatient";
+            this.txtPatient.Size = new System.Drawing.Size(100, 20);
+            this.txtPatient.TabIndex = 2;
+            // 
+            // txtPhysician
+            // 
+            this.txtPhysician.Location = new System.Drawing.Point(273, 34);
+            this.txtPhysician.Name = "txtPhysician";
+            this.txtPhysician.Size = new System.Drawing.Size(100, 20);
+            this.txtPhysician.TabIndex = 3;
+            // 
+            // btnApply
+            // 
+            this.btnApply.Location = new System.Drawing.Point(422, 34);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(75, 23);
+            this.btnApply.TabIndex = 4;
+            this.btnApply.Text = "Apply";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(223, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Dosage";
+            // 
+            // txtDosage
+            // 
+            this.txtDosage.Location = new System.Drawing.Point(273, 63);
+            this.txtDosage.Name = "txtDosage";
+            this.txtDosage.Size = new System.Drawing.Size(100, 20);
+            this.txtDosage.TabIndex = 6;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(422, 227);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form4";
+            this.ClientSize = new System.Drawing.Size(508, 262);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtDosage);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnApply);
+            this.Controls.Add(this.txtPhysician);
+            this.Controls.Add(this.txtPatient);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form4";
+            this.Text = "Treatments";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPatient;
+        private System.Windows.Forms.TextBox txtPhysician;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDosage;
+        private System.Windows.Forms.Button btnExit;
     }
 }
