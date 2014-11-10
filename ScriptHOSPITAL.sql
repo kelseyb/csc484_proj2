08d13245fb948ddfa20ff1e5a60c867ce6d34264
kelseyb <kelsey.bellew@mines.sdsmt.edu>
2014-11-10 12:18:43 -0700

    fixed the script and ran it yay!

--- ScriptHOSPITAL.sql
+++ ScriptHOSPITAL.sql
@@ -1,5 +1,5 @@
 
---USE SALES 
+--USE something possibly
 --GO
 
 -- DROP TABLES
@@ -18,7 +18,6 @@ CREATE TABLE PHYSICIAN (
 	PhysicianName varchar (100) NOT NULL,
 	Specialization varchar (100) NOT NULL,
 	Salary bigint NOT NULL
-
 )
 GO
  
@@ -29,33 +28,22 @@ CREATE TABLE PATIENT (
 	PhoneNumber bigint NOT NULL,
 	EmergencyContact varchar (100) NOT NULL,
 	AdmittedDate datetime NOT NULL,
-
+	PhysicianID bigint NOT NULL REFERENCES PHYSICIAN(PhysicianID)
 )
 GO
 
 /******  CREATE TREATMENT TABLE ******/
 CREATE TABLE TREATMENT (
-	TreatmentID bigint IDENTITY PRIMARY KEY,
+	TreatmentID bigint NOT NULL PRIMARY KEY,
 	PatientID bigint NOT NULL REFERENCES PATIENT(PatientID),
 	PhysicianID bigint NOT NULL REFERENCES PHYSICIAN(PhysicianID),
 	TreatmentName varchar (100) NOT NULL,
 	DateTreated datetime NOT NULL , /*or prescribed*/
-	TreatmentDosage int NOT NULL /*can be null if not a medicine*/
-	--dosage previously was not 'not null' but 
+	TreatmentDosage int-- NOT NULL /*can be null if not a medicine*/
 )
 GO
 
---SET IDENTITY_INSERT TREATMENT ON
---GO
-
 /******  INSERT STARTER DATA INTO TABLES ******/
-INSERT INTO PATIENT
-   VALUES(1, 'Name 1', 1112222, 'Emergency Contact 1', '10-Dec-2003', 1);
-INSERT INTO PATIENT
-   VALUES(2, 'Name 2', 1112222, 'Emergency Contact 2', '13-Dec-2003', 1);
-INSERT INTO PATIENT
-   VALUES(2, 'Name 3', 1112222, 'Emergency Contact 3', '14-Dec-2003', 2);
-   
 INSERT INTO PHYSICIAN
    VALUES(1, 'Doctor 1', 'specialization 1', 100000);
 INSERT INTO PHYSICIAN
@@ -66,17 +54,25 @@ INSERT INTO PHYSICIAN
    VALUES(4, 'Doctor 4', 'specialization 4', 400000000);
 INSERT INTO PHYSICIAN
    VALUES(5, 'Doctor 5', 'specialization 5', 5);
-   
+
+INSERT INTO PATIENT
+   VALUES(1, 'Name 1', 1112222, 'Emergency Contact 1', '10-Dec-2003', 1);
+INSERT INTO PATIENT
+   VALUES(2, 'Name 2', 1112222, 'Emergency Contact 2', '13-Dec-2003', 1);
+INSERT INTO PATIENT
+   VALUES(3, 'Name 3', 1112222, 'Emergency Contact 3', '14-Dec-2003', 2);
+
 INSERT INTO TREATMENT
    VALUES(101, 1, 1, 'DOXYCYCLIN', '10-Dec-2003', 2);
 INSERT INTO TREATMENT 
-   VALUES(102, 1, 1, 'stop eating', '10-Dec-2003', 0);
+   VALUES(102, 1, 1, 'stop eating', '10-Dec-2003', NULL);
 INSERT INTO TREATMENT 
-   VALUES(103, 2, 3, 'heart surgery', '15-Dec-2003', 0);
+   VALUES(103, 2, 3, 'heart surgery', '15-Dec-2003', NULL);
 INSERT INTO TREATMENT 
    VALUES(104, 2, 2, 'heart medication', '15-Dec-2003', 5);
 INSERT INTO TREATMENT 
    VALUES(105, 3, 1, 'anti-acids', '14-Dec-2003', 10);
 INSERT INTO TREATMENT 
-   VALUES(106, 1, 5, 'vallium', '116-Dec-2003', 3);
+   VALUES(106, 1, 5, 'vallium', '16-Dec-2003', 3);
+   
 
